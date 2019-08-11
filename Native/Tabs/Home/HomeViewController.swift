0ec81515fa1e2c75312cd399f6f9c5f2888b575f
VictorLi2 <victor.li1439@gmail.com>
2019-08-11 03:24:15 -0700

    Starting Home and Favorites

--- Native/Tabs/Home/HomeViewController.swift
+++ Native/Tabs/Home/HomeViewController.swift
@@ -1,13 +1,9 @@
 import UIKit
 
 class HomeViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setupNavigationBar(title: "Home")
-    }
-    
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
     }
 }
