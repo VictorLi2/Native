1e5bffd3dce2a034af776e766d212a50a61f8c62
VictorLi2 <victor.li1439@gmail.com>
2019-11-05 00:38:21 -0800

    Nov 4th Tasks
    Left todo:
    Culture Guide pic
    nba video
    pictures

---
+++ Native/Sections/Play/PlayHomeViewController.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+class PlayHomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupNavigationBar(title: "Play")
+
+    }
+}
---
+++ Native/Sections/Write/WriteHomeViewController.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+class WriteHomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setupNavigationBar(title: "Write")
+    }
+}
